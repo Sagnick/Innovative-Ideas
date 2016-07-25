08dab5db5308dc99977f15e14cb0dad65e5acc9e
Sagnick Banerjee <sagnickbanerjee4u@gmail.com>
2016-07-25 22:10:56 +0530

    Master- Initial Skeleton

---
+++ com.in.ideas/src/com/in/ideas/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package com.in.ideas;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "com.in.ideas.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
